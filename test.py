ed6593ca2e2a419daddc4794b50b8241e00cdab4
LB <lborozan@mathos.hr>
2022-09-14 13:07:20 +0200

    stopping criterion

--- test.py
+++ test.py
@@ -13,37 +13,50 @@ def performance(X_train, y_train, X_test, y_test, clf):
     return clf.score(X_test, y_test)
 
 # 13 cell types
-def testfunc(solver, num):
-	#load data from files
-	[data, labels, names]= load_example_data("CITEseq")
-	#[data, labels, names]= load_example_data("zeisel")
-	N,d=data.shape
+def testfunc(solver, num, fixed_genes = {}):
+        #load data from files
+        [data, labels, names]= load_example_data("CITEseq")
+        #[data, labels, names]= load_example_data("zeisel")
+        N,d=data.shape
 
-	num_markers=25
-	method='pairwise'
-	#method='centers'
-	redundancy=0.25
-	
-	markers = get_markers(data, labels, num_markers, method=method, redundancy=redundancy, solver=solver, max_constraints = num, epsilon=1)
+        num_markers=25
+        method='pairwise'
+        #method='centers'
+        redundancy=0.25
+        
+        #samples, samples_labels, idx = sample(data, labels, 0.25)
+        #eps = optimize_epsilon(np.array(samples), np.array(samples_labels), np.array(data), np.array(labels), num_markers=num_markers, method=method)
+        #print(eps)       
 
-	#accuracy=performance(data, labels, data, labels, clf)
-	#accuracy_markers=performance(data[:,markers], labels, data[:,markers], labels, clf)
+        eps = 1.14450351
+        markers = get_markers(data, labels, num_markers, method=method, redundancy=redundancy, solver=solver, max_constraints=num, epsilon=eps, fixed_genes=fixed_genes)
 
-	#print("Accuracy (whole data,", d, " markers): ", accuracy)
-	#print("Accuracy (selected", num_markers, "markers)", accuracy_markers)
-	
-	return markers
+        #accuracy=performance(data, labels, data, labels, clf)
+        #accuracy_markers=performance(data[:,markers], labels, data[:,markers], labels, clf)
 
-for i in [34073]:
-	print(i)
-	markers1 = testfunc('experimental', i)
-	markers1.sort()
-	print(markers1)
+        #print("Accuracy (whole data,", d, " markers): ", accuracy)
+        #print("Accuracy (selected", num_markers, "markers)", accuracy_markers)
+        
+        return markers
 
-# data[:870]
+def sample(data, labels, sampling_rate):
+        """subsample data"""
+        indices = []
+        for i in set(labels):
+                idxs = [x for x in range(len(labels)) if labels[x] == i]
+                n = len(idxs)
+                s = int(np.ceil(len(idxs) * sampling_rate))
+                aux = np.random.permutation(n)[0:s]
+                indices += [idxs[x] for x in aux]
+        return [data[i] for i in indices], [labels[i] for i in indices], indices
 
-# experimental
-# constraints to ~optimum - 35028, / 229241 constraints, 18 constraints left (~0.008%)
+for i in [300000]:
+        fixed_genes = {0 : 0, 7 : 1, 4 : 0}
+        # markers1 = testfunc('gurobi', i, fixed_genes)
+        markers1 = testfunc('experimental', i)
+        markers1.sort()
+        print(markers1)
 
-# gurobi/scipy
-# for 34073 another 33760 are violated
+# [7, 55, 94, 101, 104, 113, 123, 125, 127, 138, 144, 149, 157, 162, 167, 178, 179, 183, 188, 208, 211, 215, 237, 270, 345]
+# [7, 55, 94, 101, 104, 113, 123, 125, 127, 138, 144, 149, 157, 162, 167, 178, 179, 183, 188, 208, 211, 215, 237, 270, 345]
+
